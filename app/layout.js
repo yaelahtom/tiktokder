a28e32f851293cece57189ca71f1f52aec3ca292
Tomi <amandipthandi@gmail.com>
2023-11-17 10:14:48 +0700

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,12 @@
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css'
 
 const Layout = ({ children }) => {
   return (
     <html lang='en'>
-      <body className='max-h-screen bg-gray-200'>{children}</body>
+      <body className='max-h-screen bg-gray-200'>{children}
+      <Analytics />
+      </body>
     </html>
   );
 };
